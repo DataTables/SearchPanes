52ea01fea50d22a52b3ab731191d13981a31dedd
SandyDatatables <sandy@datatables.net>
2020-01-10 16:53:30 +0000

    Adding typescript file for searchPanes.dataTables to keep continuity

---
+++ src/searchPanes.dataTables.ts
@@ -0,0 +1,43 @@
+/*! Bootstrap integration for DataTables' SearchPanes
+ * ©2016 SpryMedia Ltd - datatables.net/license
+ */
+// Hack to allow TypeScript to compile our UMD
+declare var define: {
+	(string, Function): any;
+	amd: string;
+};
+(function(factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['jquery', 'datatables.net-dt', 'datatables.net-searchPanes'], function($) {
+			return factory($, window, document);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		module.exports = function(root, $) {
+			if (! root) {
+				root = window;
+			}
+
+			if (! $ || ! $.fn.dataTable) {
+				$ = require('datatables.net-dt')(root, $).$;
+			}
+
+			if (! $.fn.dataTable.searchPanes) {
+				require('datatables.net-searchPanes')(root, $);
+			}
+
+			return factory($, root, root.document);
+		};
+	}
+	else {
+		// Browser
+		factory(jQuery, window, document);
+	}
+}(function($, window, document) {
+'use strict';
+let DataTable = $.fn.dataTable;
+
+return DataTable.searchPanes;
+}));
