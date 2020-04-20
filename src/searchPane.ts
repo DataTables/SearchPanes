07a16a9a55373c4b2e038232df8595ca029df996
SandyDatatables <sandy@datatables.net>
2020-04-20 13:57:34 +0000

    Added check for case that column data is null

--- src/searchPane.ts
+++ src/searchPane.ts
@@ -751,7 +751,7 @@ export default class SearchPane {
 							if (!this.c.dataLength) {
 								displayMessage = '<span class="' + this.classes.name + '">' + data + '</span>' + pill;
 							}
-							else if (data.length > this.c.dataLength) {
+							else if (data !== null && data.length > this.c.dataLength) {
 								displayMessage = '<span class="' + this.classes.name + '">'
 												+ data.substr(0, this.c.dataLength) + '...'
 												+ '</span>'
