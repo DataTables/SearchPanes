0f5f6f2bbc26103af4f2c8ab11205d61f1491e02
Colin Marks <colin@datatables.net>
2020-04-29 13:07:11 +0100

    DEV fixed broken stateSave tests and added new test for cascade and viewTotals

--- test/integration/searchPanes.stateSave.js
+++ test/integration/searchPanes.stateSave.js
@@ -8,6 +8,17 @@ describe('searchPanes - integrations - stateSave', function() {
 
 	describe('Functional tests', function() {
 		dt.html('basic');
+
+		function checkSelection() {
+			expect($('div.dtsp-searchPane:eq(2) table tbody tr.selected td:eq(0) span.dtsp-name:eq(0)').text()).toBe(
+				'Edinburgh'
+			);
+			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe(
+				'Edinburgh'
+			);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Cedric Kelly');
+		}
+
 		it('Check selection', function() {
 			table = $('#example').DataTable({
 				dom: 'Pfrtip',
@@ -17,94 +28,213 @@ describe('searchPanes - integrations - stateSave', function() {
 
 			$('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0)').click();
 
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr.selected td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Edinburgh');
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Edinburgh');
-			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Cedric Kelly');
+			checkSelection();
 		});
 		it('... still same after reload', async function() {
 			table = $('#example').DataTable({
-                dom: 'Pfrtip',
-                destroy: true,
+				dom: 'Pfrtip',
+				destroy: true,
 				searchPanes: true,
 				stateSave: true
 			});
 
 			await dt.sleep(500);
 
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr.selected td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Edinburgh');
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Edinburgh');
-			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Cedric Kelly');
-        });
-        it('Test ordering', async function() {
-            table.state.clear();
+			checkSelection();
+		});
+
+		function checkOrdering() {
+			expect($('div.dtsp-searchPane:eq(1) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe(
+				'Technical Author'
+			);
+			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe(
+				'San Francisco'
+			);
+		}
+
+		it('Test ordering', async function() {
+			table.state.clear();
 			table = $('#example').DataTable({
-                dom: 'Pfrtip',
-                destroy: true,
+				dom: 'Pfrtip',
+				destroy: true,
 				searchPanes: true,
 				stateSave: true
 			});
 
-            await dt.sleep(500);
-            
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(1)').click();
-            $('div.dtsp-searchPane:eq(2) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(2)').click();
+			await dt.sleep(500);
 
-			expect($('div.dtsp-searchPane:eq(1) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Technical Author');
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('San Francisco');
-        });
+			$('div.dtsp-searchPane:eq(1) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(1)').click();
+			$('div.dtsp-searchPane:eq(2) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(2)').click();
+
+			checkOrdering();
+		});
+		// DD-1422
+		// it('... still same after reload', async function() {
+		// 	table = $('#example').DataTable({
+		// 		dom: 'Pfrtip',
+		// 		destroy: true,
+		// 		searchPanes: true,
+		// 		stateSave: true
+		// 	});
+
+		// 	await dt.sleep(500);
+
+		// 	checkOrdering();
+		// });
+
+		function checkSearch() {
+			expect($('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val()).toBe('Developer');
+			expect($('div.dtsp-searchPane:eq(1) table tbody tr').length).toBe(4);
+		}
+
+		it('Test searching', async function() {
+			table.state.clear();
+			table = $('#example').DataTable({
+				dom: 'Pfrtip',
+				destroy: true,
+				searchPanes: true,
+				stateSave: true
+			});
+
+			await dt.sleep(500);
+
+			$('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val('Developer');
+			$('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').trigger('input');
+
+			checkSearch();
+		});
+		// DD-1421
+		// it('... still same after reload', async function() {
+		// 	table = $('#example').DataTable({
+		// 		dom: 'Pfrtip',
+		// 		destroy: true,
+		// 		searchPanes: true,
+		// 		stateSave: true
+		// 	});
+
+		// 	await dt.sleep(500);
+
+		// 	checkSearch();
+		// });
+
+		function checkTotals(pane, exp) {
+			for (let i = 0; i < exp.length; i++) {
+				expect(
+					$('div.dtsp-searchPane:visible:eq(' + pane + ') table tbody tr:eq(' + i + ') span.dtsp-pill').text()
+				).toBe(exp[i]);
+			}
+		}
+
+		function checkCascades() {
+			checkTotals(0, ['2 (6)']);
+			checkTotals(1, ['1 (9)', '2 (12)', '1 (11)', '2 (14)']);
+			checkTotals(2, ['1 (1)', '1 (2)']);
+		}
+
+		it('Test viewtotal and cascadePanes', async function() {
+			table.state.clear();
+			table = $('#example').DataTable({
+				dom: 'Pfrtip',
+				destroy: true,
+				searchPanes: {
+					cascadePanes: true,
+					viewTotal: true
+				},
+				stateSave: true
+			});
+
+			await dt.sleep(500);
+
+			$('div.dtsp-searchPane:visible:eq(0) table tbody tr:eq(26) td:eq(0)').click();
+			$('div.dtsp-searchPane:visible:eq(1) table tbody tr:eq(1) td:eq(0)').click();
+
+			await dt.sleep(500);
+
+			checkCascades();
+		});
 		it('... still same after reload', async function() {
 			table = $('#example').DataTable({
-                dom: 'Pfrtip',
-                destroy: true,
-				searchPanes: true,
+				dom: 'Pfrtip',
+				destroy: true,
+				searchPanes: {
+					cascadePanes: true,
+					viewTotal: true
+				},
 				stateSave: true
 			});
 
-            await dt.sleep(500);
-            
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(1)').click();
-            $('div.dtsp-searchPane:eq(2) div.dtsp-topRow div.dtsp-buttonGroup button.dtsp-paneButton:eq(2)').click();
+			await dt.sleep(500);
 
-			expect($('div.dtsp-searchPane:eq(1) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('Technical Author');
-			expect($('div.dtsp-searchPane:eq(2) table tbody tr:eq(0) td:eq(0) span.dtsp-name:eq(0)').text()).toBe('San Francisco');
-        });
-        it('Test searching', async function() {
-            table.state.clear();
+			checkCascades();
+		});
+
+		function checkCustom() {
+			expect($('div.dtsp-searchPane:visible:eq(3) table tbody tr:eq(0)').hasClass('selected')).toBe(true);
+			expect($('#example tbody tr').length).toBe(2);
+			expect($('#example tbody tr:eq(0) td:eq(0)').text()).toBe('Airi Satou');
+			expect($('#example tbody tr:eq(1) td:eq(0)').text()).toBe('Garrett Winters');
+		}
+
+		it('Test custom panes', async function() {
+			table.state.clear();
 			table = $('#example').DataTable({
-                dom: 'Pfrtip',
-                destroy: true,
-				searchPanes: true,
+				dom: 'Pfrtip',
+				destroy: true,
+				searchPanes: {
+					panes: [
+						{
+							name: 'Custom',
+							options: [
+								{
+									label: 'Accountants from Tokyo',
+									value: function(rowData, rowIdx) {
+										return rowData[1] === 'Accountant' && rowData[2] === 'Tokyo';
+									}
+								}
+							]
+						}
+					]
+				},
 				stateSave: true
 			});
 
-            await dt.sleep(500);
-            
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val('Developer');
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').trigger('input');
+			await dt.sleep(500);
 
-            expect($('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val()).toBe('Developer');
-			expect($('div.dtsp-searchPane:eq(1) table tbody tr').length).toBe(4);
-        });
+			$('div.dtsp-searchPane:visible:eq(3) table tbody tr:eq(0) td:eq(0)').click();
+
+			await dt.sleep(500);
+
+			checkCustom();
+		});
 		it('... still same after reload', async function() {
 			table = $('#example').DataTable({
-                dom: 'Pfrtip',
-                destroy: true,
-				searchPanes: true,
+				dom: 'Pfrtip',
+				destroy: true,
+				searchPanes: {
+					panes: [
+						{
+							name: 'Custom',
+							options: [
+								{
+									label: 'Accountants from Tokyo',
+									value: function(rowData, rowIdx) {
+										return rowData[1] === 'Accountant' && rowData[2] === 'Tokyo';
+									}
+								}
+							]
+						}
+					]
+				},
 				stateSave: true
 			});
 
-            await dt.sleep(500);
-            
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val('Developer');
-            $('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').trigger('input');
+			await dt.sleep(500);
 
-            expect($('div.dtsp-searchPane:eq(1) div.dtsp-topRow input').val()).toBe('Developer');
-			expect($('div.dtsp-searchPane:eq(1) table tbody tr').length).toBe(4);
-        });
-		// TK COLIN should add similar tests for custom panes
+			checkCustom();
+		});
 	});
 
-	describe('stateSave when searchPanes not enables originally', function() {
+	describe('stateSave when searchPanes not enabled originally', function() {
 		dt.html('basic');
 		it('No searchPanes originally', function() {
 			table = $('#example').DataTable({
