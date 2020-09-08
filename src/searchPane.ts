58a2c04de6be94c03a143baefe6f487adace72cd
SandyDataTables <sandy@datatables.net>
2020-09-08 09:31:08 +0000

    fix: When ordering using viewTotal, the number of shown records is now a sub sort
    Jira Issue DD-1632

--- src/searchPane.ts
+++ src/searchPane.ts
@@ -985,7 +985,13 @@ export default class SearchPane {
 					{
 						className: 'dtsp-countColumn ' + this.classes.badgePill,
 						data: 'total',
+						orderData: [1, 2],
 						targets: 1,
+						visible: false
+					},
+					{
+						data: 'shown',
+						targets: 2,
 						visible: false
 					}
 				],
