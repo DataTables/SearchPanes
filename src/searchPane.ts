9a8e6887fa1d491cc1ffd78bd39cea9c15ce6949
sandyDatatables <sandy@datatables.net>
2020-10-01 08:27:11 +0000

    Fix: Perform loose type checking in the search rather than strict
    Jira Issue DD-1686

--- src/searchPane.ts
+++ src/searchPane.ts
@@ -1542,7 +1542,8 @@ export default class SearchPane {
 				}
 			}
 			// otherwise if the two filter values are equal then return true
-			else if (filter === colSelect.filter) {
+			// Loose type checking incase number type in column comparing to a string
+			else if (filter == colSelect.filter) {
 				return true;
 			}
 		}
