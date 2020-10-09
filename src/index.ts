caffb71db7324a2133be0a74e95abf336052e711
Allan Jardine <allan@datatables.net>
2020-10-09 15:04:21 +0000

    Dev: Ready for 1.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchPanes 1.2.1-dev
+/*! SearchPanes 1.2.1
  * 2019-2020 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     SearchPanes
  * @description Search Panes for DataTables columns
- * @version     1.2.1-dev
+ * @version     1.2.1
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @copyright   Copyright 2019-2020 SpryMedia Ltd.
  *
