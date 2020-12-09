f2ce294cd36bfdfda1e0d69088d4ae406b26ca1b
sandyDatatables <sandy@datatables.net>
2020-12-09 16:23:54 +0000

    dev: Update version to 1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchPanes 1.2.2
+/*! SearchPanes 1.3.0
  * 2019-2020 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     SearchPanes
  * @description Search Panes for DataTables columns
- * @version     1.2.2
+ * @version     1.3.0
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @copyright   Copyright 2019-2020 SpryMedia Ltd.
  *
