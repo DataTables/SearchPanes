0c91cd825cf2135e5329a8535b42a7ce56c8e197
SandyDatatables <sandy@datatables.net>
2021-06-16 09:24:07 +0000

    dev: Update gitignore to make nightly rebuild automatically by removing autgen type files

--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-/*! SearchPanes 1.3.0
- * 2019-2020 SpryMedia Ltd - datatables.net/license
- */
-export {};
--- types/searchPanes.bootstrap.d.ts
+++
@@ -1,7 +0,0 @@
-/*! Bootstrap integration for DataTables' SearchPanes
- * ©2016 SpryMedia Ltd - datatables.net/license
- */
-declare let define: {
-	amd: string;
-	(stringValue: any, Function: any): any;
-};
