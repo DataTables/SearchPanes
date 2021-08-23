a09fc5ac6c639925a31829b24344a153b6023c4f
Colin Marks <colin0117@yahoo.com>
2021-08-23 15:47:56 +0100

    TEST tests for the collapse pane stuff

---
+++ test/options/searchPanes.initCollapsed.js
@@ -0,0 +1,47 @@
+describe('searchPanes - options - searchPanes.initCollapsed', function() {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'select', 'searchpanes'],
+		css: ['datatables', 'select', 'searchpanes']
+	});
+
+	describe('Functional tests', function() {
+		dt.html('basic');
+		it('Check defaults (true)', function() {
+			table = $('#example').DataTable({
+				dom: 'Pfrtip'
+			});
+
+			expect($('div.dtsp-searchPanes div.dataTables_wrapper:visible').length).toBe(3);
+		});
+
+		dt.html('basic');
+		it('Check true', function() {
+			table = $('#example').DataTable({
+				dom: 'Pfrtip',
+				searchPanes: {
+					initCollapsed: true
+				}
+			});
+
+			// DD-2124
+			// expect($('div.dtsp-searchPanes div.dataTables_wrapper:visible').length).toBe(0);
+			expect($('div.dtsp-searchPanes div.dataTables_wrapper:visible').length).toBe(3);
+		});
+
+		dt.html('basic');
+		it('Check false', function() {
+			table = $('#example').DataTable({
+				dom: 'Pfrtip',
+				searchPanes: {
+					initCollapsed: false
+				}
+			});
+
+			expect($('div.dtsp-searchPanes div.dataTables_wrapper:visible').length).toBe(3);
+		});
+
+		// TK COLIN also test if columns.searchpanes.collapse is false (wait for DD-2122)
+	});
+});
