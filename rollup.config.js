92f5726f8e2d67dbb0c4dad877c6a44076cb481c
SandyDatatables <sandy@datatables.net>
2021-12-20 13:43:05 +0000

    fix: Fix rollup bundling issues

--- rollup.config.js
+++ rollup.config.js
@@ -9,50 +9,5 @@ export default [
 			format: 'iife'
 		},
 		plugins: [resolve()]
-	},
-	{
-		input: 'src/searchPanes.bootstrap.js',
-		moduleContext: () => 'window',
-		output: {
-			file: process.env.OUT + '/js/searchPanes.bootstrap.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/searchPanes.bootstrap4.js',
-		moduleContext: () => 'window',
-		output: {
-			file: process.env.OUT + '/js/searchPanes.bootstrap4.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/searchPanes.dataTables.js',
-		moduleContext: () => 'window',
-		output: {
-			file: process.env.OUT + '/js/searchPanes.dataTables.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/searchPanes.foundation.js',
-		moduleContext: () => 'window',
-		output: {
-			file: process.env.OUT + '/js/searchPanes.foundation.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/searchPanes.semanticui.js',
-		moduleContext: () => 'window',
-		output: {
-			file: process.env.OUT + '/js/searchPanes.semanticui.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
 	}
 ];
