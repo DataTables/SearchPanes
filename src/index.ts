c82ac12bf17d4199cdc45cb1806592bba80840b3
Allan Jardine <allan@datatables.net>
2022-05-12 10:38:22 +0000

    2.0.1 release

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! SearchPanes 2.0.0
+/*! SearchPanes 2.0.1
  * 2019-2022 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     SearchPanes
  * @description Search Panes for DataTables columns
- * @version     2.0.0
+ * @version     2.0.1
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @copyright   Copyright 2019-2022 SpryMedia Ltd.
  *
